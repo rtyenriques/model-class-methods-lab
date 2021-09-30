8058b19a82b7585debdb695ddc5be4b78c69ab6b
= <rtyenriques@gmail.com>
2021-09-30 12:41:29 -0700

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -4,10 +4,13 @@ class Classification < ActiveRecord::Base
 
   def self.my_all
     # all
+    all
   end
 
   def self.longest
     # Boat.longest.classifications
+
+    Boat.longest.classifications
   end
 
 end
